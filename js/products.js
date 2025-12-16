a7f4030ad8c542fdec0ea6f6e4a7c4610c0179a6
Default8914 <114364229+Default8914@users.noreply.github.com>
2025-12-16 14:19:50 +0300

    Delete js directory

--- js/products.js
+++
@@ -1,5 +0,0 @@
-export async function loadProducts() {
-  const res = await fetch("./assets/products.json");
-  if (!res.ok) throw new Error("Не удалось загрузить товары: assets/products.json");
-  return await res.json();
-}
